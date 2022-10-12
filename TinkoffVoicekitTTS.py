d418769ffcc2d0b1d394738583a054719ddcdc85
Vladimir (vapronva) <git@vprw.ru>
2022-10-12 15:19:43 +0300

    add: licence, ds; change: move `auth.py`; edit: main func for tts generation

--- TinkoffVoicekitTTS.py
+++ TinkoffVoicekitTTS.py
@@ -1,43 +1,53 @@
+from typing import Optional, Tuple
 from tinkoff.cloud.tts.v1 import tts_pb2_grpc, tts_pb2
-from auth import authorization_metadata
+from tinkoff.auth import authorization_metadata
 import grpc
 import os
 import wave
+from pathlib import Path
 
-endpoint = os.environ.get("VOICEKIT_ENDPOINT") or "api.tinkoff.ai:443"
-api_key = os.environ["VOICEKIT_API_KEY"]
-secret_key = os.environ["VOICEKIT_SECRET_KEY"]
+_VKC_API_ENDPOINT = os.environ.get("VOICEKIT_ENDPOINT") or "api.tinkoff.ai:443"
+_VCK_API_KEY = os.environ["VOICEKIT_API_KEY"]
+_VCK_SECRET_KEY = os.environ["VOICEKIT_SECRET_KEY"]
 
-sample_rate = 48000
+SAMPLE_RATE = 48000
 
 
-def build_request():
-    return tts_pb2.SynthesizeSpeechRequest(
+def process_text_to_speech(
+    text: str,
+    selected_voice: str,
+    output_file: Path,
+    sample_rate: int = SAMPLE_RATE,
+    api_key: str = _VCK_API_KEY,
+    secret_key: str = _VCK_SECRET_KEY,
+    endpoint_api: str = _VKC_API_ENDPOINT,
+) -> Tuple[Optional[float]]:
+    request = tts_pb2.SynthesizeSpeechRequest(
         input=tts_pb2.SynthesisInput(
-            text="И мысли тоже тяжелые и медлительные, падают неторопливо и редко "
-            "одна за другой, точно песчинки в разленившихся песочных часах."
+            text=text,
+        ),
+        voice=tts_pb2.VoiceSelectionParams(
+            name=selected_voice,
         ),
         audio_config=tts_pb2.AudioConfig(
             audio_encoding=tts_pb2.LINEAR16,
             sample_rate_hertz=sample_rate,
         ),
     )
-
-
-with wave.open("synthesized.wav", "wb") as f:
-    f.setframerate(sample_rate)
-    f.setnchannels(1)
-    f.setsampwidth(2)
-
-    stub = tts_pb2_grpc.TextToSpeechStub(
-        grpc.secure_channel(endpoint, grpc.ssl_channel_credentials())
-    )
-    request = build_request()
-    metadata = authorization_metadata(api_key, secret_key, "tinkoff.cloud.tts")
-    responses = stub.StreamingSynthesize(request, metadata=metadata)
-    for key, value in responses.initial_metadata():
-        if key == "x-audio-duration-seconds":
-            print("Estimated audio duration is {:.2f} seconds".format(float(value)))
-            break
-    for stream_response in responses:
-        f.writeframes(stream_response.audio_chunk)
+    with wave.open(output_file.absolute().__str__(), mode="wb") as f:
+        f.setframerate(sample_rate)
+        f.setnchannels(1)
+        f.setsampwidth(2)
+        stub = tts_pb2_grpc.TextToSpeechStub(
+            grpc.secure_channel(endpoint_api, grpc.ssl_channel_credentials())
+        )
+        metadata = authorization_metadata(api_key, secret_key, "tinkoff.cloud.tts")
+        responses = stub.StreamingSynthesize(request, metadata=metadata)
+        AUDIO_DURATION = None
+        for key, value in responses.initial_metadata():
+            if key == "x-audio-duration-seconds":
+                AUDIO_DURATION = float(value)
+                break
+        for stream_response in responses:
+            f.writeframes(stream_response.audio_chunk)
+    return (AUDIO_DURATION,)
